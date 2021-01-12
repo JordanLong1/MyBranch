ed41cab4d9b54f56d5baf123626c8ff22babbf7b
Jordan <jordanleelong5@gmail.com>
2021-01-12 14:39:04 -0800

    added questions and options

---
+++ src/QuestionBank/index.js
@@ -0,0 +1,68 @@
+export const questionsAndOptions = [
+    {questionId: 1, 
+        questionTitle: 'Question 1', 
+        options: [
+            { 
+           optionTitle: 'Option 1', 
+             pointsTo: 2
+            }, 
+            {
+            optionTitle: 'Option 2', 
+            pointsTo: 3                
+            }, 
+            {
+            optionTitle: 'Option 3', 
+            pointsTo: 4
+            }
+        ], 
+    }, 
+    {
+        questionId: 2, 
+        questionTitle: 'Question 2', 
+        options: [
+            {optionTitle: 'Option 1', 
+            pointsTo: 5
+            }, 
+            {
+                optionTitle: 'Option 2', 
+                pointsTo: 6
+            }
+        
+             ]
+        }, 
+
+    {
+        questionId: 3, 
+        questionTitle: 'Question 3', 
+        options: [
+            {
+             optionTitle: 'Option 1', 
+            pointsTo: null
+            }, 
+        {
+            optionTitle: 'Option 2', 
+            pointsTo: null
+        }
+        ]
+    }, 
+
+    {
+        questionId: 4, 
+        questionTitle: 'Question 4', 
+        options: [
+            {
+                optionTitle: 'Option 1',
+                pointsTo: 7
+            }, 
+            {
+                optionTitle: 'Option 2', 
+                pointsTo: 8
+            }, 
+            {
+                optionTitle: 'Option 3', 
+                pointsTo: 9
+            }
+        ]
+    }
+]
+
