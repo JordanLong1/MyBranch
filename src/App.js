c4127e4a760550b0c3faec2b2bfe3f02faedcba2
Jordan <jordanleelong5@gmail.com>
2021-01-12 21:18:23 -0800

    added landing page route and question display route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import DisplayQuestions from './Components/DisplayQuestions'
-
+import {BrowserRouter, Route} from 'react-router-dom'
+import { LandingPage } from './Components/LandingPage';
 
 function App() {
   return (
    <div className='app'>
-   
-    <DisplayQuestions />
+     <BrowserRouter>
+       <Route exact path='/' component={LandingPage} />
+        <Route exact path='/displayquestion' component={DisplayQuestions}/>
+   </BrowserRouter>
    </div>
   );
 }
