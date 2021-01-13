d8b951f8e9a5eb6841d5bf841b4f146a4748f80c
Jordan <jordanleelong5@gmail.com>
2021-01-12 21:18:01 -0800

    added classNames and more structure to question container

--- src/Components/DisplayQuestions.jsx
+++ src/Components/DisplayQuestions.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {questionsAndOptions} from '../QuestionBank/index'
+
 export default function DisplayQuestion() {
 
     const [currentQuestion, setCurrentQuestion] = useState(questionsAndOptions[0]);
@@ -7,7 +8,9 @@ export default function DisplayQuestion() {
 
     const handleClick = (e) => {
         let pointsToValue = e.target.id; 
+
         pointsToValue = parseInt(e.target.id, 10)
+
         
         const nextQuestion = questionsAndOptions.find(question => question.questionId === pointsToValue)
         setCurrentQuestion(nextQuestion)
@@ -15,11 +18,15 @@ export default function DisplayQuestion() {
     }
 
     return (
-        <div>
-            <h4>{currentQuestion.questionTitle}</h4>
+        <div className='question-container'>
+
+        <div className='question-section'>
+            <h4 className='question-header'>{currentQuestion.questionTitle}</h4>
             <ul>
                 {currentQuestion.options.map((option, index) => <li key={index} id={option.pointsTo} onClick={handleClick}>{option.optionTitle}</li>)}
             </ul>
+        </div>
+        
         </div>
     )
 }
