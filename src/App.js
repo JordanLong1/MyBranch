ed431bcab5889f6694934c39755005ea6d58c215
Jordan <jordanleelong5@gmail.com>
2021-01-13 13:14:42 -0800

    extracted routing info.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import DisplayQuestions from './Components/DisplayQuestions'
-import {BrowserRouter, Route} from 'react-router-dom'
-import { LandingPage } from './Components/LandingPage';
 
 function App() {
   return (
    <div className='app'>
-     <BrowserRouter>
-       <Route exact path='/' component={LandingPage} />
-        <Route exact path='/displayquestion' component={DisplayQuestions}/>
-   </BrowserRouter>
+  
    </div>
   );
 }
