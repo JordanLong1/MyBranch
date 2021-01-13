7ce5aa0643b2c3ca667ae33e96a6945e0da81a35
Jordan <jordanleelong5@gmail.com>
2021-01-13 13:23:33 -0800

    added the most basic navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function NavBar() {
     return (
-        <nav></nav>
+   
+            <section className='header'>
+                <section className='header-top'>
+                    <section className='header-top_home-email-route'>
+                    <h1>Branching Scenario</h1>
+                    <a href='/'>Home</a>
+                    <span onClick={() => window.location.href = "mailto:jordanleelong5@gmail.com"}>Email</span>
+                    </section>
+                </section>
+            </section>
+        
     )
 }
