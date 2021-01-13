e433432c862943b554a7731fc1371afe0d4a474f
Jordan <jordanleelong5@gmail.com>
2021-01-12 21:17:33 -0800

    added landing page

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'; 
+import { useHistory } from "react-router-dom";
+export const LandingPage = () => {
+
+    const history = useHistory(); 
+
+    const handleClick = () => {
+      
+        history.push('/displayquestion');
+    };
+
+    return (
+        <header className='landing-page-container'>
+        <h1>Welcome to Jordan Long's Branching Scenario</h1>
+        <p>We are going to cover the four different learning styles!</p>
+        <button onClick={handleClick}>Get Started</button>
+        </header>
+    ); 
+};
