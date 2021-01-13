176f1c2bb30b0a1e2a3fdde0a8038ffbb0787b08
Jordan <jordanleelong5@gmail.com>
2021-01-13 11:03:04 -0800

    added question images

--- src/QuestionBank/index.js
+++ src/QuestionBank/index.js
@@ -1,6 +1,17 @@
+import firstImage from '../Learning Images/first-learning-img.png'
+import brainMap from '../Learning Images/brain-map.jpg'
+import rocketLearning from '../Learning Images/rocket-learning.jpg'
+import pencilLearning from '../Learning Images/pencil-learning.jpg'
+import hybridLearning from '../Learning Images/hybrid-learning.jpg'
+import jugglingSkills from '../Learning Images/juggling-skills.jpg'
+import classroomLearning from '../Learning Images/classroom-learning.png'
+import partnerLearning from '../Learning Images/partner-learning.jpeg'
+import learningInSteps from '../Learning Images/learning-4-steps.jpg'
+
 export const questionsAndOptions = [
     {questionId: 1, 
         questionTitle: 'How do you think you learn best?', 
+        questionImage: firstImage,
         options: [
             { 
            optionTitle: 'Online learning', // almost finished need something for null.
@@ -22,6 +33,7 @@ export const questionsAndOptions = [
     {
         questionId: 2, 
         questionTitle: 'What works better for you when studying for a test?', 
+        questionImage: brainMap,
         options: [
             {optionTitle: 'Memorizing contents of index cards', 
             pointsTo: 5, 
@@ -40,6 +52,7 @@ export const questionsAndOptions = [
     {
         questionId: 3, 
         questionTitle: 'Which method would you prefer over the other?', 
+        questionImage: pencilLearning,
         options: [
             {
              optionTitle: 'Learning by doing. Hands on experience', 
@@ -57,6 +70,7 @@ export const questionsAndOptions = [
     {
         questionId: 4, 
         questionTitle: 'When choosing a career area of study, I would:', 
+        questionImage: rocketLearning,
         options: [
             {
                 optionTitle: 'Work with designs, maps or charts.',
@@ -79,6 +93,7 @@ export const questionsAndOptions = [
     {
         questionId: 5, 
         questionTitle: 'When you approach learning something new, which method do you prefer?', 
+        questionImage: hybridLearning,
         options: [
             {
                 optionTitle: 'Watching a video explanation.',
@@ -95,6 +110,7 @@ export const questionsAndOptions = [
     {
         questionId: 6, // B points here.
         questionTitle: 'You want to put together a new desk from Ikea, how would you approach the task?', 
+        questionImage: jugglingSkills,
         options: [
             {
                 optionTitle: 'Start with the directions given.',
@@ -111,6 +127,7 @@ export const questionsAndOptions = [
     {
         questionId: 7, 
         questionTitle: 'What sort of school assignments do you enjoy the most? ', 
+        questionImage: classroomLearning,
         options: [
             {
                 optionTitle: 'Memorizing assignments, video lectures, watching and listening to the teacher give a lecture.',
@@ -133,6 +150,7 @@ export const questionsAndOptions = [
     {
         questionId: 8, 
         questionTitle: 'What is the best way for you to remember your friends phone number?', 
+        questionImage: partnerLearning,
         options: [
             {
                 optionTitle: 'Picture the numbers on your phone.',
@@ -150,6 +168,7 @@ export const questionsAndOptions = [
     {
         questionId: 9, 
         questionTitle: 'A website has a video showing how to make a special graph or chart. There is a person speaking, some lists and words describing what to do and some diagrams. I would learn most from:', 
+        questionImage: learningInSteps,
         options: [
             {
                 optionTitle: 'Watching the actions',
