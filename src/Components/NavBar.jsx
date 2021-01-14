d2b3ddcd3cd7663637a47b3c48325f7040b77876
Jordan <jordanleelong5@gmail.com>
2021-01-13 23:59:46 -0800

    removed email

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,7 +8,6 @@ export default function NavBar() {
                     <section className='header-top_home-email-route'>
                     <h1>Branching Scenario</h1>
                     <a href='/'>Home</a>
-                    <span onClick={() => window.location.href = "mailto:jordanleelong5@gmail.com"}>Email</span>
                     </section>
                 </section>
             </section>
