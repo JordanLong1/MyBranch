7f563cfb8e6368f029aa12945a15699a62087955
Jordan <jordanleelong5@gmail.com>
2021-01-13 22:43:39 -0800

    added an end of branch display

--- src/Components/EndOfBranchDisplay.jsx
+++ src/Components/EndOfBranchDisplay.jsx
@@ -3,14 +3,20 @@ import PropTypes from 'prop-types';
 
 
 export const EndOfBranchDisplay = ({results}) => {
+        console.log('yooooo', results)
     
     return (
-        <div>
-    {results.map(result => console.log(result))}
-        </div>
+        <section>
+        <h2>Congragulations! You have made it to the end of the session.</h2>
+        <p>Your learning style results are available below.</p>
+        <p>These results are accumulated by which option you chose for each question.</p>
+        <section>
+            <p>It looks like you're a {results} learner!</p>
+        </section>
+        </section>
     )
 }
 
 EndOfBranchDisplay.propTypes = {
-    results: PropTypes.array.isRequired
+    results: PropTypes.string.isRequired
 };
