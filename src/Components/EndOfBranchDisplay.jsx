022bf10ccc8e64a0f1ab1bcc8afa23c10d3dd110
Jordan <jordanleelong5@gmail.com>
2021-01-13 23:38:37 -0800

    iterated over results array to determine which options were chose the most

--- src/Components/EndOfBranchDisplay.jsx
+++ src/Components/EndOfBranchDisplay.jsx
@@ -3,7 +3,35 @@ import PropTypes from 'prop-types';
 
 
 export const EndOfBranchDisplay = ({results}) => {
-        console.log('yooooo', results)
+
+        let learningStyle; 
+         let count = {}
+        let compare = 0
+        let mostFrequent; 
+    
+        for (let i = 0; i < results.length; i++) {
+            let current = results[i];
+    
+            if (count[current] === undefined) {
+                count[current] = 1;
+            } else {
+                count[current] = count[current] + 1;
+            }
+    
+            if (count[current] > compare) {
+                compare = count[current];
+                mostFrequent = results[i];
+            };
+        };
+       if (mostFrequent === 'A') {
+           learningStyle = 'Visual';
+       } else if (mostFrequent === 'B') {
+           learningStyle = 'Reading / Writing';
+       } else if (mostFrequent === 'C') {
+           learningStyle = 'Hybrid. Combination of both';
+       };
+
+    
     
     return (
         <section>
@@ -11,12 +39,12 @@ export const EndOfBranchDisplay = ({results}) => {
         <p>Your learning style results are available below.</p>
         <p>These results are accumulated by which option you chose for each question.</p>
         <section>
-            <p>It looks like you're a {results} learner!</p>
+            <p>It looks like you're a {learningStyle} learner!</p>
         </section>
         </section>
     )
 }
 
 EndOfBranchDisplay.propTypes = {
-    results: PropTypes.string.isRequired
+    results: PropTypes.array.isRequired
 };
