c9b19247447db72657bba71c50e2e04d2dff9d3f
Jordan <jordanleelong5@gmail.com>
2021-01-13 23:38:12 -0800

    removed unneeded divs, added ternary to render EndOfBranchDisplay

--- src/Components/DisplayQuestions.jsx
+++ src/Components/DisplayQuestions.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {questionsAndOptions} from '../QuestionBank/index';
+import { EndOfBranchDisplay } from './EndOfBranchDisplay';
 import QuestionHandler from './QuestionHandler'
 
 export default function DisplayQuestion() {
@@ -9,40 +10,31 @@ export default function DisplayQuestion() {
     const [results, setResults] = useState([]);
     
     const handleClick = (event) => {
-
         handleClickHelper(event); // this function was getting bulky, needed some abstraction
-
         let pointsToValue = event.target.id; // this value dictates the next question. 
-
         pointsToValue = parseInt(event.target.id, 10);
-
-        let nextQuestion = questionsAndOptions.find(question => question.questionId === pointsToValue)
+        let nextQuestion = questionsAndOptions.find(question => question.questionId === pointsToValue);
         pointsToValue !== 0 ?  setCurrentQuestion(nextQuestion) : setEndOfBranch(true);
     };
 
     const handleClickHelper = (event) => {
         const answerType = event.target.attributes[1].nodeValue; 
-
         let resultArray = [...results];
-
         resultArray.push(answerType);
-
         setResults(resultArray);
-       
-    }
+    };
 
     const getOptionsIntoList = () => {
        return currentQuestion.options.map((option, index) => {
            return <li key={index} id={option.pointsTo} typeofanswer={option.typeOfAnswer} onHandleClick={handleClick}>{option.optionTitle}</li>
-        })
-    }
+        });
+    };
 
     
     return (
         <div className='question-container'>
-        <div className='question-section'>
-        <QuestionHandler questionTitle={currentQuestion.questionTitle} questionImg={currentQuestion.questionImage} children={getOptionsIntoList}/>
-        </div>
+          <QuestionHandler questionTitle={currentQuestion.questionTitle} questionImg={currentQuestion.questionImage} children={getOptionsIntoList}/>
+             {endOfBranch === true ? <EndOfBranchDisplay results={results} /> : <div></div>}
         </div>
     );
 };
