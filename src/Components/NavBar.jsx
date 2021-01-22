19d1906f1f6958cb8362abb3ca4c2d197b1ee60a
Jordan <jordanleelong5@gmail.com>
2021-01-21 22:42:37 -0800

    changed sections to divs

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,12 +4,12 @@ export default function NavBar() {
     return (
    
             <section className='header'>
-                <section className='header-top'>
-                    <section className='header-top_home-email-route'>
+                <div className='header-top'>
+                    <div className='header-top_home-email-route'>
                     <h1>Branching Scenario</h1>
                     <a href='/'>Home</a>
-                    </section>
-                </section>
+                    </div>
+                </div>
             </section>
         
     )
