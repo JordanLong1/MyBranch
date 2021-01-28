f058ca6d96ff6a8c9a6723ab4461a1beaeb0e40b
Jordan <jordanleelong5@gmail.com>
2021-01-27 22:38:35 -0800

    trying to fix hosting problem

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Routes from './components/Routes';
+import Routes from './components/Routes.jsx';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
